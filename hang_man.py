385456798d5a79d04fa55a1ed77b8e64c368e958
wanjalajodham@gmail.com <jodham>
2020-12-01 23:02:19 +0300

    first commit

---
+++ hang_man.py
@@ -0,0 +1,12 @@
+import random
+import time
+
+#welcoming the user
+name = input("what is your name")
+print("helloo," + name,"Time to play hangman")
+print("")
+#wait for 1 second
+time.sleep(2)
+
+print("Start guessing......")
+time.sleep(0.5)
