b31eecde1584cde236ac685f32b5a209751e3e8c
wanjalajodham@gmail.com <jodham>
2020-12-02 05:41:38 +0300

    add existing file

--- hang_man.py
+++ hang_man.py
@@ -1,12 +1,24 @@
 import random
 import time
 
-#welcoming the user
-name = input("what is your name")
-print("helloo," + name,"Time to play hangman")
-print("")
-#wait for 1 second
-time.sleep(2)
+Fruits = ['pea','mango','quava','pineapple','apple','grapes','orange','melon']
+Superheroes =['iron man','captain america','thor','antman','statham']
 
-print("Start guessing......")
+userGuessList = []
+userGuesses = []
+playGame = True
+category = ""
+continueGame = "Y"
+
+#welcoming the user
+name = input("Enter your name")
+print("helloo," + name.capitalize(),"Time to play hangman")
+print("")
+#wait for 2 second
+time.sleep(2)
+print(">>>>The objective of the game is to guess the secret word chosen by the computer.")
+time.sleep(2)
+print(">>>You can guess only one letter at a time. Dont forget to press 'Enter key' after each guess")
+time.sleep(2)
+print(">>>Have fun......")
 time.sleep(0.5)
