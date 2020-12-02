d9bc8b0889de05e1291b094f64fa23a95520ee4f
wanjalajodham@gmail.com <jodham>
2020-12-02 23:04:04 +0300

    new

--- hang_man.py
+++ hang_man.py
@@ -1,52 +1,104 @@
-import random
-import time
-
-Fruits = ['pea','mango','quava','pineapple','apple','grapes','orange','melon']
-Superheroes =['iron man','captain america','thor','antman','statham']
-
-userGuessList = []
+import random, time
+fruits = ['pear', 'mango', 'apple', 'banana', 'apricot', 'pineapple','cantaloupe', 'grapefruit','jackfruit','papaya']
+superHeroes = ['hawkeye', 'robin', 'Galactus', 'thor', 'mystique', 'superman', 'deadpool', 'vision', 'sandman', 'aquaman']
+userGuesslist = []
 userGuesses = []
 playGame = True
 category = ""
 continueGame = "Y"
 
-#welcoming the user
 name = input("Enter your name")
-print("helloo," + name.capitalize(),"Time to play hangman")
-print("")
-#wait for 2 second
+print("Hello", name.capitalize(), "let's start playing Hangman!")
+time.sleep(1)
+print("The objective of the game is to guess the secret word chosen by the computer.")
+time.sleep(1)
+print("You can guess only one letter at a time. Don't forget to press 'enter key' after each guess.")
 time.sleep(2)
-print(">>>>The objective of the game is to guess the secret word chosen by the computer.")
-time.sleep(2)
-print(">>>You can guess only one letter at a time. Dont forget to press 'Enter key' after each guess")
-time.sleep(2)
-print(">>>Have fun......")
-time.sleep(0.5)
-
-#choosing the secret word
+print("Let the fun begin!")
+time.sleep(1)
 
 while True:
-    if category.upper() == 'S':
-        secretWord = random.choice(Superheroes)
-        break
-    elif category.upper() =='F':
-        secretWord = random.choice(Fruits)
-        break
-    else:
-        category = input("please select a valid category: F for Fruits / S for Super-Heroes; X to exit")
+    #Choosing the Secret word
+    while True:
+        if category.upper() == 'S':
+            secretWord = random.choice(superHeroes)
+            break
+        elif category.upper() == 'F':
+            secretWord = random.choice(fruits)
+            break
+        else:
+            category = input("Please select a valid categary: F for Fruits / S for Super-Heroes; X to exit")
+
         if category.upper() == 'X':
-            print("bye. see u next time!!")
+            print("Bye. See you next time!")
             playGame = False
             break
-if playGame:
-    secretWordList = list(secretWord)
-    attempts = (len(secretWord) + 2)
 
-    userGuessList = []
-    
+    if playGame:
+        secretWordList = list(secretWord)
+        attempts = (len(secretWord) + 2)
 
-
-
-
+        #Utility function to print User Guess List
+        def printGuessedLetter():
+            print("Your Secret word is: " + ''.join(userGuesslist))
 
 
+        #Adding blank lines to userGuesslist to create the blank secret word
+        for n in secretWordList:
+            userGuesslist.append('_')
+        printGuessedLetter()
+
+        print("The number of allowed guesses for this word is:", attempts)
+
+
+        #starting the game
+        while True:
+
+            print("Guess a letter:")
+            letter = input()
+
+            if letter in userGuesses:
+                print("You already guessed this letter, try something else.")
+
+            else:
+                attempts -= 1
+                userGuesses.append(letter)
+                if letter in secretWordList:
+                    print("Nice guess!")
+                    if attempts > 0:
+                        print("You have ", attempts, 'guess left!')
+                    for i in range(len(secretWordList)):
+                        if letter == secretWordList[i]:
+                            letterIndex = i
+                            userGuesslist[letterIndex] = letter.upper()
+                    printGuessedLetter()
+
+                else:
+                    print("Oops! Try again.")
+                    if attempts > 0:
+                        print("You have ", attempts, 'guess left!')
+                    printGuessedLetter()
+
+
+            #Win/loss logic for the game
+            joinedList = ''.join(userGuesslist)
+            if joinedList.upper() == secretWord.upper():
+                print("Yay! you won.")
+                break
+            elif attempts == 0:
+                print("Too many Guesses!, Sorry better luck next time.")
+                print("The secret word was: "+ secretWord.upper())
+                break
+
+        #Play again logic for the game
+        continueGame = input("Do you want to play again? Y to continue, any other key to quit")
+        if continueGame.upper() == 'Y':
+            category = input("Please select a valid categary: F for Fruits / S for Super-Heroes")
+            userGuesslist = []
+            userGuesses = []
+            playGame = True
+        else:
+            print("Thank You for playing. See you next time!")
+            break
+    else:
+        break
